1de51d97949c889d9d0f31533ed2deb4baa09fc6
KonradOliwer <kcomrp@gmail.com>
2014-06-01 05:42:28 +0200

    usefull model fix

--- src/jakprzejade/model2/Path.java
+++ src/jakprzejade/model2/Path.java
@@ -1,16 +1,17 @@
 package jakprzejade.model2;
 
 /**
- * Container representing possible way you can take from given node 
- * (eg. from Sokola take bus D at 900 to stop Halera, which takes 5 minute)
+ * Container representing possible way you can take from given node (eg. from Sokola take bus D at
+ * 900 to stop Halera, which takes 5 minute)
  *
  * @author KonradOliwer
  */
 public class Path {
 
-    String line; //line identifier
-    int startTime; //time of day in minutes
-    int last; //how long it takes to get to target node
-    Node from;
-    Node to;
+    public DayType dayType;
+    public String line; //line identifier
+    public int startTime; //time of day in minutes
+    public int last; //how long it takes to get to target node
+    public Node from;
+    public Node to;
 }
