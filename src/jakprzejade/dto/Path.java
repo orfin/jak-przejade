093c8e406897a8041bb03feea3e338caaa2a8a30
Antoni Orfin <a.orfin@imagin.com.pl>
2014-06-14 20:28:56 +0200

    Added test route finder and DTO objects for response.

---
+++ src/jakprzejade/dto/Path.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jakprzejade.dto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Antoni Orfin <orfin@poczta.fm>
+ */
+public class Path {
+    public String vehicleName;
+    public String vehicleType;
+    
+    public ArrayList<Node> nodes = new ArrayList<Node>();
+}
